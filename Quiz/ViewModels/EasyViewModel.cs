b193d79c5748099176468eee028919151057d3e3
ionmlad99 <mladinoiion99@gmail.com>
2020-06-30 00:34:25 +0300

    Select difficulty questions

--- Quiz/ViewModels/EasyViewModel.cs
+++ Quiz/ViewModels/EasyViewModel.cs
@@ -3,6 +3,7 @@ using Quiz.Models;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Windows.Markup;
 using System.Xml;
 using static Quiz.Models.QuestionsAndAnswers;
 
@@ -11,24 +12,43 @@ namespace Quiz.ViewModels
     public class EasyViewModel : BindableBase
     {
         public List<QuestionsAndAnswers> QuizQuestions = new List<QuestionsAndAnswers>();
-        private void ReadDataBase()
+
+        public string _NameQuestion;
+        public void ReadDataBase()
         {
             XmlDataDocument xmlDataDocument = new XmlDataDocument();
-            xmlDataDocument.Load(@"C:\Users\Ion Ml\source\repos\Quiz\Quiz\Models\Questions.xml");
-            foreach(XmlNode node in xmlDataDocument.DocumentElement)
+            xmlDataDocument.Load(@"C:\\Users\\Ion Ml\\source\\repos\\Quiz\\Quiz\\Models\\Questions.xml");
+            foreach (XmlNode node in xmlDataDocument.DocumentElement)
             {
-                string QuestionName = node.Attributes[0].Value;
-                string CorrectAnswer = node["CorrectAnswer"].Value;
-                string WrongAnswer1 = node["WrongAnswer1"].Value;
-                string WrongAnswer2 = node["WrongAnswer2"].Value;
-                string WrongAnswer3 = node["WrongAnswer3"].Value;
-                string Difficulty = node["Difficulty"].Value;
-                if (Difficulty == "Easy")
+                string QuestionName = node.Attributes[0].InnerText;
+                string CorrectAnswer = node["CorrectAnswer"].InnerText;
+                string WrongAnswer1 = node["WrongAnswer1"].InnerText;
+                string WrongAnswer2 = node["WrongAnswer2"].InnerText;
+                string WrongAnswer3 = node["WrongAnswer3"].InnerText;
+                string Difficulty = node["Difficulty"].InnerText;
+                if (Difficulty.Equals("Easy"))
                 {
                     QuestionsAndAnswers questionsAndAnswers = new QuestionsAndAnswers(QuestionName, CorrectAnswer, WrongAnswer1, WrongAnswer2, WrongAnswer3, Difficulty);
                     QuizQuestions.Add(questionsAndAnswers);
                 }
             }
         }
+
+        public EasyViewModel()
+        {
+            ReadDataBase();
+            for (int i = 0; i < QuizQuestions.Count; i++)
+            {
+                QuestionsAndAnswers values = QuizQuestions[i];
+                _NameQuestion = values.QuestionName;
+            }
+        }
+        public string NameQuestion
+        {
+            get { return _NameQuestion; }
+            set { SetProperty(ref _NameQuestion, value); }
+        }
     }
 }
+
+
--- Quiz/Views/EasyView.xaml.cs
+++ Quiz/Views/EasyView.xaml.cs
@@ -1,4 +1,5 @@
-﻿using Quiz.ViewModels;
+﻿using Quiz.Models;
+using Quiz.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Text;
